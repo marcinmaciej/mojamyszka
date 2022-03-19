08c41af763ab80d38fff515de74f0bbf23df75cb
marcinmaciej <marcinmaciej.g@gmail.com>
2022-03-19 17:45:53 +0100

    Initial commit

--- mojamyszka.js
+++ mojamyszka.js
@@ -1,35 +1,13 @@
-console.log("Moja myszka działa.");
-window.setInterval(function () {
-    let ads_module,
-        popup_module;
-    ads_module = document.querySelector(".video-ads,.ytp-ad-module");
-    popup_module = document.querySelector("ytd-popup-container");
-    if ((null !== ads_module) && (0 !== ads_module.children.length)) {
-        let abs = ads_module.querySelector(".ytp-ad-skip-button,.ytp-button");
-        let abc = ads_module.querySelector(".ytp-ad-overlay-close-button");
-        if (null !== abs) {
-            abs.click();
-        } else if (null !== abc) {
-            abc.click();
-        }
-    }
-    if ((null !== popup_module) && (0 !== popup_module.children.length)) {
-        let paper_dialog = popup_module.querySelector("paper-dialog");
-        if ((null !== paper_dialog) && (0 !== paper_dialog.children.length)) {
-            let paper_dialog_visibility = window.getComputedStyle(document.querySelector("paper-dialog"), null).getPropertyValue("display");
-            if ("none" !== paper_dialog_visibility) {
-                let abp = paper_dialog.querySelector("#button");
-                if ((null !== abp) && ("Tak" === abp.getAttribute("aria-label"))) {
-                    abp.click();
-                    console.log("Dialog kliknięty.");
-                }
-            }
-        }
-
-    }
-}, 300);
-
-//Obecnie działająca wersja ostatnia poprawka 15.03.2022r.
+/**
+ * @author  Created by Marcin Guziołek on 25.08.2020 r.
+ *
+ * @brief Obecnie działająca wersja ostatnia poprawka 19.03.2022r.
+ *
+ * @details Obsługuje wszystkie trzy uciążliwe przypadki:
+ * przycisk 'Pomiń reklamę', baner reklamowy oraz komunikat wstrzymujący oglądanie video.
+ * Każde obsłużenie jest logowane w konsoli przeglądarki.
+ *
+ */
 console.log("Moja myszka działa.");
 window.setInterval(function () {
     let ads_module,
@@ -43,7 +21,7 @@ window.setInterval(function () {
 
         if (null !== abs) {
             abs.click();
-            console.log("Kliknięty.");
+            console.log("Przycisk liknięty.");
         } else if (null !== abc) {
             abc.click();
             console.log("Baner kliknięty.");
@@ -60,7 +38,7 @@ window.setInterval(function () {
 
             if ("none" !== paper_dialog_visibility) {
 
-                let abp = paper_dialog.querySelector("#button");
+                let abp = paper_dialog.querySelector('#confirm-button').querySelector("#button");
 
                 if (null !== abp && (abp.getAttribute("aria-label") === "Tak")) {
                     abp.click();
